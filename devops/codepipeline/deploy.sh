630b20b92df7c614acb6e916a2ee959217d3b94b
ishak0604 <Isha.k@meyicloud.com>
2026-01-19 09:09:21 +0000

    Fix CodePipeline IAM, tagging, and auto deploy

--- devops/codepipeline/deploy.sh
+++ devops/codepipeline/deploy.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 set -e
 
-if [ -z "$1" ]; then
-  echo "Usage: ./deploy.sh <dev|prod>"
+ENV=$1
+
+if [ -z "$ENV" ]; then
+  echo "Usage: ./deploy.sh dev|prod"
   exit 1
 fi
-
-ENV=$1
-export ENV
 
 echo "🔄 Loading environment: $ENV"
 source ../env/${ENV}_env.sh
 
-echo "🚀 Deploying CodePipeline for $ENV"
+export AWS_DEFAULT_REGION=$AWS_REGION
+
+STACK_NAME=ecs-cpu-codepipeline-$ENV
+
+echo "🚀 Deploying CodePipeline stack: $STACK_NAME"
 
 aws cloudformation deploy \
-  --region "$AWS_REGION" \
+  --stack-name $STACK_NAME \
   --template-file pipeline.yaml \
-  --stack-name "ecs-cpu-pipeline-$ENV" \
   --capabilities CAPABILITY_NAMED_IAM \
-  --tags Project=learning \
   --parameter-overrides \
-    Environment="$ENV" \
-    GitHubConnectionArn="$GITHUB_CONNECTION_ARN" \
-    RepositoryName="$GITHUB_REPO" \
-    BranchName="$BRANCH" \
-    ArtifactBucket="$ARTIFACT_BUCKET" \
-    ImageRepoName="$IMAGE_REPO_NAME" \
-    VpcId="$VPC_ID" \
-    PublicSubnet1="$PUBLIC_SUBNET1" \
-    PublicSubnet2="$PUBLIC_SUBNET2" \
-    TaskExecutionRoleArn="$TASK_EXECUTION_ROLE_ARN"
-
-
-echo "✅ Pipeline deployed successfully"
+    Environment=$ENV \
+    GitHubConnectionArn=$GITHUB_CONNECTION_ARN \
+    RepositoryName=$GITHUB_REPO \
+    BranchName=$BRANCH \
+    ArtifactBucket=$ARTIFACT_BUCKET \
+    ImageRepoName=$IMAGE_REPO_NAME \
+    VpcId=$VPC_ID \
+    PublicSubnet1=$PUBLIC_SUBNET1 \
+    PublicSubnet2=$PUBLIC_SUBNET2 \
+    TaskExecutionRoleArn=$TASK_EXECUTION_ROLE_ARN
